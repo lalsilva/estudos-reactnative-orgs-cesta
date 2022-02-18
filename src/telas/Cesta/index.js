d0dcfc14d06343a1e948e564c4e6d4370f60ea0d
Luiz Silva <lasilva35@stefanini.com>
2022-02-18 19:39:19 -0300

    incluida lista de itens

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
 import Topo from './componentes/Topo';
 import Detalhes from './componentes/Detalhes';
+import Itens from './componentes/Itens';
 
-export default function Cesta({ topo, detalhes }) {
+export default function Cesta({ topo, detalhes, itens }) {
     return (
-        <>
+        <ScrollView>
             <Topo {...topo} />
 
             <View style={estilos.cesta}>
                 <Detalhes {...detalhes} />
+                <Itens {...itens} />
             </View>
-        </>
+        </ScrollView>
     )
 }
 
