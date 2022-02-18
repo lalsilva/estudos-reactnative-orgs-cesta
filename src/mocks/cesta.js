4638cca4ad08b54f6bdf2ec520e714b7da232255
Luiz Silva <lasilva35@stefanini.com>
2022-02-18 18:55:27 -0300

    incluido nó de itens no mock da cesta

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,15 +1,47 @@
 import logo from '../../assets/logo.png';
 
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brocolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abobora.png';
+
 const cesta = {
     topo: {
-        titulo: 'Detalhes da Cesta'
+        titulo: "Detalhe da Cesta",
     },
     detalhes: {
-        nome: 'Cesta de Verduras',
+        nome: "Cesta de Verduras",
         logoFazenda: logo,
-        nomeFazenda: 'Jenny Jack Farm',
-        descricao: 'Uma cesta com produtos selecionados cuidadosamente da fazendo direto para a cozinha',
-        preco: 'R$ 40,00'
+        nomeFazenda: "Jenny Jack Farm",
+        descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
+        preco: "R$ 40,00",
+        botao: "Comprar",
+    },
+    itens: {
+        titulo: "Itens da cesta",
+        lista: [
+            {
+                nome: "Tomate",
+                imagem: tomate,
+            },
+            {
+                nome: "Brócolis",
+                imagem: brocolis,
+            },
+            {
+                nome: "Batata",
+                imagem: batata,
+            },
+            {
+                nome: "Pepino",
+                imagem: pepino,
+            },
+            {
+                nome: "Abóbora",
+                imagem: abobora,
+            }
+        ]
     }
 }
 
